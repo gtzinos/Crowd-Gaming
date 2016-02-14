7158ae86ba05e2504d2709eb8c4113f701147ac9
George Tzinos <geotzinos@gmail.com>
2016-02-14 11:32:46 +0200

    Database model

--- pages/Public/Home/HomePageView.php
+++ pages/Public/Home/HomePageView.php
@@ -1,11 +1,10 @@
 <?php if($section == "CSS") : ?>
-	<head>
 	<link rel="stylesheet" href="/pages/Public/Home/css/HomePage.css">
-</head>
 <?php elseif($section == "JAVASCRIPT") : ?>
 
 <?php elseif($section == "MAIN_CONTENT" ) : ?>
 
 	<h1>Home Page</h1>
+
 
 <?php endif; ?>
