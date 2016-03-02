ed5859fad04e08d86e5613124d25869256ff9343
stavros.skourtis <stavros.skourtis@gmail.com>
2016-03-02 14:30:24 +0200

    ActivationView fix

--- app/view/public/ActivationView.php
+++ app/view/public/ActivationView.php
@@ -9,11 +9,11 @@
 		if(get("error-code") == 0)
 		{
 			echo "<h1>Register completed successfull.</h1>";
-			echo "<p>Now you can log in on our system.</p>"
+			echo "<p>Now you can log in on our system.</p>";
 		}
 		else if(get("error-code") == 1)
 		{
-			echo "<h1>Invalid activation link.</h1>"
+			echo "<h1>Invalid activation link.</h1>";
 		}
 		else if(get("error-code") == 2)
 		{
