89e6f7fc70ec07190568176642c75fb0878bc78c
stavros.skourtis <stavros.skourtis@gmail.com>
2016-03-05 11:30:03 +0200

    Controller parameters fix.

--- core/router/RouteDispatcher.php
+++ core/router/RouteDispatcher.php
@@ -10,6 +10,12 @@
 				Get the correct Controller
 			 */
 			$route["controller"] = Routes::get($params[0]);
+			
+			foreach ($params as $key => $value) {
+				if( empty($value) )
+					unset($params[$key]);
+			}
+
 			$route["parameters"] = $params;
 
 			return $route;
