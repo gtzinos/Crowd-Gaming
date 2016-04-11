abab75b02e5b69b939ce418e4bdc7f58363e9359
George Tzinos <geotzinos@gmail.com>
2016-04-11 22:49:30 +0300

    {FIX}Questionnaire Groups View responsive design

--- app/view/examiner/QuestionnaireGroupsView.php
+++ app/view/examiner/QuestionnaireGroupsView.php
@@ -10,7 +10,7 @@
   <legend class="text-center header">Questionnaire Groups</legend>
   <!-- ShortCut Buttons -->
   <div class="form-group has-feedback">
-    <div class="col-xs-offset-0 col-xs-1 col-md-1">
+    <div class="col-xs-offset-0 col-xs-2">
       <a class="btn btn-primary gt-submit" href="<?php echo LinkUtils::generatePageLink('create-questionnaire-groups') ?>">Add</a>
     </div>
   </div>
@@ -20,63 +20,61 @@
     */
 
   ?>
-  <div class="list-group col-xs-offset-0 col-xs-12">
-    <a href="#" class="list-group-item col-md-offset-1 col-md-10">
-      <div class="qgroup-name col-xs-12">
+  <div class="list-group">
+    <a href="#" class="list-group-item col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10">
+      <div class="col-xs-12">
         <h4 class="list-group-item-heading">First List Group Item Heading</h4>
       </div>
       <div class="col-xs-12" style="margin-top:3%;padding:0px">
-        <div class="col-xs-3">
+        <div class="col-xs-12 col-sm-4 col-md-3" style="padding:0px">
           <button class="btn" type="button">New Question</button>
         </div>
-        <div class="col-xs-1" style="padding:0px">
+        <div class="col-xs-12 col-sm-2 col-md-1" style="padding:0px">
           <button class="btn" type="button">Edit</button>
         </div>
-        <div class="col-xs-2" style="padding:0px">
+        <div class="col-xs-12 col-sm-2 col-md-2" style="padding:0px">
           <button class="btn" type="button">Delete</button>
         </div>
-        <div class="col-xs-offset-3 col-xs-3 ">
+        <div class="col-xs-12 col-sm-offset-1 col-sm-3 col-md-offset-3 col-md-3">
           <button class="btn btn-link" type="button">Questions <span class="badge">2</span></button>
         </div>
       </div>
     </a>
 
-    <a href="#" class="list-group-item col-md-offset-1 col-md-10">
-      <div class="qgroup-name col-xs-12">
+    <a href="#" class="list-group-item col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10">
+      <div class="col-xs-12">
         <h4 class="list-group-item-heading">First List Group Item Heading</h4>
       </div>
       <div class="col-xs-12" style="margin-top:3%;padding:0px">
-        <div class="col-xs-3">
+        <div class="col-xs-12 col-sm-4 col-md-3" style="padding:0px">
           <button class="btn" type="button">New Question</button>
         </div>
-        <div class="col-xs-1" style="padding:0px">
+        <div class="col-xs-12 col-sm-2 col-md-1" style="padding:0px">
           <button class="btn" type="button">Edit</button>
         </div>
-        <div class="col-xs-2" style="padding:0px">
+        <div class="col-xs-12 col-sm-2 col-md-2" style="padding:0px">
           <button class="btn" type="button">Delete</button>
         </div>
-        <div class="col-xs-offset-3 col-xs-3 ">
+        <div class="col-xs-12 col-sm-offset-1 col-sm-3 col-md-offset-3 col-md-3">
           <button class="btn btn-link" type="button">Questions <span class="badge">2</span></button>
         </div>
       </div>
     </a>
-
-
-    <a href="#" class="list-group-item col-md-offset-1 col-md-10">
-      <div class="qgroup-name col-xs-12">
+    <a href="#" class="list-group-item col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10">
+      <div class="col-xs-12">
         <h4 class="list-group-item-heading">First List Group Item Heading</h4>
       </div>
       <div class="col-xs-12" style="margin-top:3%;padding:0px">
-        <div class="col-xs-3">
+        <div class="col-xs-12 col-sm-4 col-md-3" style="padding:0px">
           <button class="btn" type="button">New Question</button>
         </div>
-        <div class="col-xs-1" style="padding:0px">
+        <div class="col-xs-12 col-sm-2 col-md-1" style="padding:0px">
           <button class="btn" type="button">Edit</button>
         </div>
-        <div class="col-xs-2" style="padding:0px">
+        <div class="col-xs-12 col-sm-2 col-md-2" style="padding:0px">
           <button class="btn" type="button">Delete</button>
         </div>
-        <div class="col-xs-offset-3 col-xs-3 ">
+        <div class="col-xs-12 col-sm-offset-1 col-sm-3 col-md-offset-3 col-md-3">
           <button class="btn btn-link" type="button">Questions <span class="badge">2</span></button>
         </div>
       </div>
