2d9cb04777c227d7267a2b76506bf73c5799c75c
George Tzinos <geotzinos@gmail.com>
2016-04-11 21:50:25 +0300

    {NEW} Example for QuestionnaireGroupsView design

--- app/view/examiner/QuestionnaireGroupsView.php
+++ app/view/examiner/QuestionnaireGroupsView.php
@@ -1,4 +1,5 @@
 <?php if($section == "CSS") : ?>
+  <link rel="stylesheet" href="<?php print LinkUtils::generatePublicLink("css/examiner/QuestionnaireGroupsList.css"); ?>">
 <?php elseif($section == "JAVASCRIPT") : ?>
   <!-- Google Maps -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1P_ouQbGN0ehtuSm58zqrYxS-YPk4XwM" type="text/javascript"></script>
@@ -13,6 +14,71 @@
       <a class="btn btn-primary gt-submit" href="<?php echo LinkUtils::generatePageLink('create-questionnaire-groups') ?>">Add</a>
     </div>
   </div>
+  <?php
+    /*
+        For each Questionnaire Group
+    */
 
-</div>
+  ?>
+  <div class="list-group col-xs-offset-0 col-xs-12">
+    <a href="#" class="list-group-item col-md-offset-1 col-md-10">
+      <div class="qgroup-name col-xs-12">
+        <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+      </div>
+      <div class="col-xs-12" style="margin-top:3%;padding:0px">
+        <div class="col-xs-3">
+          <button class="btn" type="button">New Question</button>
+        </div>
+        <div class="col-xs-1" style="padding:0px">
+          <button class="btn" type="button">Edit</button>
+        </div>
+        <div class="col-xs-2" style="padding:0px">
+          <button class="btn" type="button">Delete</button>
+        </div>
+        <div class="col-xs-offset-3 col-xs-3 ">
+          <button class="btn btn-link" type="button">Questions <span class="badge">2</span></button>
+        </div>
+      </div>
+    </a>
+
+    <a href="#" class="list-group-item col-md-offset-1 col-md-10">
+      <div class="qgroup-name col-xs-12">
+        <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+      </div>
+      <div class="col-xs-12" style="margin-top:3%;padding:0px">
+        <div class="col-xs-3">
+          <button class="btn" type="button">New Question</button>
+        </div>
+        <div class="col-xs-1" style="padding:0px">
+          <button class="btn" type="button">Edit</button>
+        </div>
+        <div class="col-xs-2" style="padding:0px">
+          <button class="btn" type="button">Delete</button>
+        </div>
+        <div class="col-xs-offset-3 col-xs-3 ">
+          <button class="btn btn-link" type="button">Questions <span class="badge">2</span></button>
+        </div>
+      </div>
+    </a>
+
+
+    <a href="#" class="list-group-item col-md-offset-1 col-md-10">
+      <div class="qgroup-name col-xs-12">
+        <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+      </div>
+      <div class="col-xs-12" style="margin-top:3%;padding:0px">
+        <div class="col-xs-3">
+          <button class="btn" type="button">New Question</button>
+        </div>
+        <div class="col-xs-1" style="padding:0px">
+          <button class="btn" type="button">Edit</button>
+        </div>
+        <div class="col-xs-2" style="padding:0px">
+          <button class="btn" type="button">Delete</button>
+        </div>
+        <div class="col-xs-offset-3 col-xs-3 ">
+          <button class="btn btn-link" type="button">Questions <span class="badge">2</span></button>
+        </div>
+      </div>
+    </a>
 <?php endif; ?>
