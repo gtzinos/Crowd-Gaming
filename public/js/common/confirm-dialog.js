e680861b63016877af5aa8f27825c021d7ab5e90
George Tzinos <geotzinos@gmail.com>
2016-04-21 17:11:47 +0300

    {NEW} dynamic method  for Confirm dialog library added

--- public/js/common/confirm-dialog.js
+++ public/js/common/confirm-dialog.js
@@ -4,21 +4,55 @@
   Api : http://craftpip.github.io/jquery-confirm/#api
 */
 
-$('button.gt-confirm-message').confirm({
-		icon: 'fa fa-warning', // Title icon
-    title: 'Please confirm',
+var confirm_dialog;
+
+function display_confirm_dialog(title,body,confirmButtonClass,cancelButtonClass,theme,onConfirm,onCancel)
+{
+ 	confirm_dialog = $.confirm({
+		title: title,
+		content: body,
+		contentLoaded: function(){
+		},
+		icon: '',
+			confirmButton: 'Yes',
+		cancelButton: 'Cancel',
+		confirmButtonClass: confirmButtonClass,
+		cancelButtonClass: cancelButtonClass,
+		theme: 'black',
+		animation: 'zoom',
+		closeAnimation: 'scale',
+		animationSpeed: 500,
+		animationBounce: 1.2,
+		keyboardEnabled: false,
+		rtl: false,
+		confirmKeys: [13], // ENTER key
+		cancelKeys: [27], // ESC key
+		container: 'body',
+		confirm: function () {
+			if(onConfirm != "")
+			{
+				eval(onConfirm);
+			}
+
+		},
+		cancel: function () {
+			if(onCancel != "")
+			{
+				eval(onCancel);
+			}
+
+		},
+		backgroundDismiss: false,
+		autoClose: false,
 		closeIcon: true,
-	 	closeIconClass: 'fa fa-close', // or 'glyphicon glyphicon-remove'
-		columnClass: 'col-xs-6 col-xs-offset-3',
-    //content: '',
-		confirmButton: 'Yes i agree',
-    cancelButton: 'NOT now',
-		confirmButtonClass: 'btn-warning', // btn-primary btn-inverse btn-warning btn-info btn-danger btn-success
-    cancelButtonClass: 'btn-info', // btn-primary btn-inverse btn-warning btn-info btn-danger btn-success
-    confirm: function(){
-        //$.alert('Done');
-    },
-    cancel: function(){
-        $.alert('You have canceled it.')
-    }
-});
+    closeIconClass: 'fa fa-close', // or 'glyphicon glyphicon-remove'
+		columnClass: 'col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1',
+		onOpen: function(){
+		},
+		onClose: function(){
+		},
+		onAction: function(){
+		}
+	});
+
+}
