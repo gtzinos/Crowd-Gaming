fbc3e1e2f4d0661f3bac6ac549128a9c94c36bc4
StavrosSkourtis <stavros.skourtis@gmail.com>
2016-04-21 20:25:48 +0300

    CreateQuestionnaireController

--- app/controller/examiner/CreateQuestionnaireController.php
+++ app/controller/examiner/CreateQuestionnaireController.php
@@ -3,12 +3,17 @@
 	include_once '../app/model/mappers/actions/ParticipationMapper.php';
 	include_once '../libs/htmlpurifier-4.7.0/HTMLPurifier.auto.php';
 
-	class CreateQuestionnaireController extends Controller{
+	class CreateQuestionnaireController extends Controller
+	{
 		
-		public function init(){
-			if( isset($this->params[1]) && $this->params[1]=="ajax"){
+		public function init()
+		{
+			if( isset($this->params[1]) && $this->params[1]=="ajax")
+			{
 				$this->setHeadless(true);
-			}else{
+			}
+			else
+			{
 				global $_CONFIG;
 
 				$this->setTemplate($_CONFIG["BASE_TEMPLATE"]);
@@ -19,7 +24,8 @@
 			}			
 		}
 
-		public function run(){
+		public function run()
+		{
 
 			/*
 				Response Codes (Ajax)
@@ -29,9 +35,13 @@
 				2			: Description Validation error
 				3			: Message Required Error
 				4			: Database Error
+				5			: Name already exists
 			 */
 			
-			if( isset($this->params[1], $_POST["name"] ,  $_POST["description"] , $_POST["message_required"] ) && $this->params[1]=="ajax" ){
+			if( isset($this->params[1], $_POST["name"] ,  $_POST["description"] , $_POST["message_required"] ) && $this->params[1]=="ajax" )
+			{
+
+				$questionnaireMapper = new QuestionnaireMapper;
 
 				$name = htmlspecialchars($_POST["name"] , ENT_QUOTES);
 
@@ -41,19 +51,30 @@
 
 				$messageRequired = $_POST["message_required"];
 
-				if( strlen($name) < 3 ){
+				if( strlen($name) < 3 )
+				{
 					
 					print 1; // Name Validation error
 
-				}else if( strlen($description) < 30 ){
+				}
+				else if(  $questionnaireMapper->nameExists( $name ) )
+				{
+					print 5; // Name already exists
+				}
+				else if( strlen($description) < 30 )
+				{
 					
 					print 2; // Descriptin validation error
 
-				}else if( $messageRequired != "no" && $messageRequired != "yes"){
+				}
+				else if( $messageRequired != "no" && $messageRequired != "yes")
+				{
 
 					print 3; // Message required error
 
-				}else{
+				}
+				else
+				{
 
 					$questionnaire = new Questionnaire;
 
@@ -72,10 +93,11 @@
 					$playerParticipation->setUserId( $_SESSION["USER_ID"]);
 					$playerParticipation->setParticipationType( 1 );
 
-					$questionnaireMapper = new QuestionnaireMapper;
+					
 					$participationMapper = new ParticipationMapper;
 
-					try{
+					try
+					{
 						DatabaseConnection::getInstance()->startTransaction();
 
 
@@ -96,7 +118,9 @@
 						DatabaseConnection::getInstance()->commit();
 						print 0; // All ok
 
-					}catch(DatabaseException $ex){
+					}
+					catch(DatabaseException $ex)
+					{
 						
 						DatabaseConnection::getInstance()->rollback();
 						print 4; // Database Error
