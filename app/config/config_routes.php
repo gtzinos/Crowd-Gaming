a5f2fab6063e135d0a37419e73b587d9f6bce93a
StavrosSkourtis <stavros.skourtis@gmail.com>
2016-04-25 20:00:42 +0300

    CreateUserParticipation AJAX Controller

--- app/config/config_routes.php
+++ app/config/config_routes.php
@@ -52,6 +52,7 @@
 	Routes::add( 'get-users-from-question-group' , 'examiner/ajax/GetUsersFromQuestionGroupController.php' , $_USER_LEVEL["EXAMINER"]);
 	Routes::add( 'publish-questionnaire-request' , 'examiner/ajax/PublishQuestionnaireApplicationController.php' , $_USER_LEVEL["EXAMINER"] );
 	Routes::add( 'delete-questionnaire-participation' , 'examiner/ajax/DeleteQuestionnaireParticipationController.php' , $_USER_LEVEL["EXAMINER"]);
+	Routes::add( 'create-questionnaire-participation' , 'examiner/ajax/CreateQuestionnaireParticipationController.php' , $_USER_LEVEL["EXAMINER"]);
 	Routes::add( 'handle-questionnaire-request' , "examiner/ajax/HandleQuestionnaireRequestController.php" , $_USER_LEVEL["EXAMINER"] );
 	Routes::add( 'get-users-from-questionnaire' , 'examiner/ajax/GetUsersFromQuestionnaireController.php' , $_USER_LEVEL["EXAMINER"] ); 
 	Routes::add( 'get-all-question-groups' , 'examiner/ajax/GetAllQuestionGroupsController.php' ,$_USER_LEVEL["EXAMINER"]);
