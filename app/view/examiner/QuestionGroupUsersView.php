b45f68b6fddc4ba2843b0ff9fe03b8cdf44e3c9b
George Tzinos <giwrgos.0101@gmail.com>
2016-04-27 21:29:06 +0300

    {NEW} Question group users (DESIGN)

--- app/view/examiner/QuestionGroupUsersView.php
+++ app/view/examiner/QuestionGroupUsersView.php
@@ -1,5 +1,5 @@
-<? if($section == "QUESTION_LIST") : ?>
-<div class="modal fade" id="question-group-user" role="dialog">
+<? if($section == "QUESTION_GROUP_USERS") : ?>
+<div class="modal fade" id="question-group-users" role="dialog">
   <div class="modal-dialog modal-md">
     <div class="modal-content">
        <div class="modal-header">
@@ -9,32 +9,32 @@
        <div class="modal-body container-fluid">
 
          <div class="form-group row">
-           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-4">
-             	<select id="single-group-dropdown" class="selectpicker" data-live-search="true" title="Select a question group">
+           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-2 col-sm-8">
+             	<select id="single-group-dropdown" class="selectpicker" data-live-search="true" data-width="100%" title="Select a question group">
 
              	</select>
            </div>
-           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-4">
-             	<button class="btn" onclick="find_participants()">Find participants</button>
-           </div>
          </div>
          <br>
          <br>
          <div class="form-group row">
-           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-4">
-             <select id="question-group-users-dropdown" class="selectpicker" data-live-search="true" title="Select some users" multiple data-actions-box="true" multiple data-selected-text-format="count > 2">
+           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-2 col-sm-5">
+             <select id="questionnaire-users-dropdown" class="selectpicker" data-live-search="true" data-width="100%" title="Select questionnaire users" multiple data-actions-box="true" multiple data-selected-text-format="count > 2">
 
              </select>
            </div>
-           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-4">
-             <select id="multiple-group-dropdown" class="selectpicker" data-live-search="true" title="Select a question group" multiple data-actions-box="true" multiple data-selected-text-format="count > 2">
-
-             </select>
+           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-3">
+             <button type="button" style="width:100%" class="btn btn-primary">Insert users</button>
            </div>
          </div>
          <div class="form-group row">
-           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-4">
-               <input type="button" class="btn btn-primary" value="Save Users" onclick="save_users()">
+           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-2 col-sm-5">
+             <select id="question-group-users-dropdown" class="selectpicker" data-live-search="true" data-width="100%" title="Select group users" multiple data-actions-box="true" multiple data-selected-text-format="count > 2">
+
+             </select>
+           </div>
+           <div class="col-xs-offset-0 col-xs-12 col-sm-offset-0 col-sm-3">
+             <button type="button" style="width:100%" class="btn btn-danger">Delete users</button>
            </div>
          </div>
        </div>
