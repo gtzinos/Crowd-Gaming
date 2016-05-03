5bb533a2b26560812ea51dee60d6e94516635fb3
StavrosSkourtis <stavros.skourtis@gmail.com>
2016-05-03 13:57:12 +0300

    GetAvailableCoordinators

---
+++ app/controller/moderator/ajax/GetAvailableCoordinatorsController.php
@@ -0,0 +1,42 @@
+<?php
+	include_once '../app/model/mappers/user/UserMapper.php';
+
+	class GetAvailableCoordinatorsController extends Controller
+	{
+		public function init()
+		{
+			$this->setOutputType(OutputType::JsonView);
+		}
+
+		public function run()
+		{
+
+
+			if( $_POST["questionnaire-id"] )
+			{
+
+				$userMapper = new UserMapper;
+
+				$users = $userMapper->findAvailableCoordinators($_POST["questionnaire-id"]);
+
+				$usersJson = array();
+				foreach ($users as $user) 
+				{
+					$arrayItem["id"] = $user->getId();
+					$arrayItem["name"] = $user->getName();
+					$arrayItem["surname"] = $user->getSurname();
+					$arrayItem["email"] = $user->getEmail();
+					$arrayItem["gender"] = $user->getGender() == 0 ? "male" : "female";
+					$arrayItem["country"] = $user->getCountry();
+					$arrayItem["city"] = $user->getCity();
+					$arrayItem["address"] = $user->getAddress();
+					$arrayItem["phone"] = $user->getPhone(); 
+
+					$usersJson[] = $arrayItem;
+				}
+
+				$this->setOutput("response_code",0);
+				$this->setOutput("users" , $usersJson);
+			}
+		}
+	}
