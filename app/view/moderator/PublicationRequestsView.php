341b9848dd47d08b429ea4f3d11af591b7fcc699
George Tzinos <geotzinos@gmail.com>
2016-05-06 09:51:34 +0300

    {FIX} File names

--- app/view/moderator/PublicationRequestsView.php
+++ app/view/moderator/PublicationRequestsView.php
@@ -10,19 +10,13 @@
       <div class='list-group-item col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10' id='qitem" + questionnaires[i].id + "'>
           <div class='col-xs-12'>
               <div class='col-xs-6'>
-                  <h4 class='list-group-item-heading'>Georgios Tzinos</h4>
+                  <h4 class='list-group-item-heading'>Αντικειμενοστρεφής προγραμματισμός Εξάμηνο 2 ΑΤΕΙΘ</h4>
               </div>
               <div class='col-xs-offset-3 col-xs-3'>
                   <h5 class='list-group-item-heading'> 20/12/2016</h5>
               </div>
           </div>
           <div class='col-xs-12'>
-            <div class='col-xs-12'>
-                <h5 class='list-group-item-heading'> Type : Player</h5>
-            </div>
-            <div class='col-xs-4 col-sm-5 col-md-4'>
-                <h5 class='list-group-item-heading'> Game : Questionnaire 1</h5>
-            </div>
               <div class='col-xs-3 col-sm-offset-4 col-sm-3 col-md-2'>
                 <div class='dropdown'>
                     <span class='dropdown-toggle btn btn-default' type='button' data-toggle='dropdown' onclick='questionnaire_id = " + questionnaires[i].id + "; questionnaire_index = " + i + ";'>Actions
