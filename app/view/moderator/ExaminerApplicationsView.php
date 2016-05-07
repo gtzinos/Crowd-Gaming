8c554d0063b9fb8f5fd8a7d0396eb19d2fa717bb
George Tzinos <geotzinos@gmail.com>
2016-05-07 20:16:45 +0300

    {NEW} Examiner application (Accept / decline ajax request)

--- app/view/moderator/ExaminerApplicationsView.php
+++ app/view/moderator/ExaminerApplicationsView.php
@@ -5,10 +5,15 @@
   <script src="<?php print LinkUtils::generatePublicLink("js/common/notification-box.js"); ?>"> </script>
   <script src="<?php print LinkUtils::generatePublicLink("js/moderator/ExaminerApplications.js"); ?>"> </script>
 <?php elseif($section == "MAIN_CONTENT" ) : ?>
+  <?php
+      echo "  <script>
+                var user_page = '" . LinkUtils::generatePageLink('user') . "';
+              </script>";
+  ?>
   <legend class="text-center header">Examiner applications</legend>
 
   <div class="container-fluid">
-    <div class="list-group" id="examiner-aplications-list">
+    <div class="list-group" id="examiner-applications-list">
 
     </div>
   </div>
