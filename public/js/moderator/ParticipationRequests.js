209936c0c303e73fbf72b3bf417e1bff2c4deb3e
George Tzinos <geotzinos@gmail.com>
2016-05-07 15:24:50 +0300

    {FIX} Design options for participation requests

--- public/js/moderator/ParticipationRequests.js
+++ public/js/moderator/ParticipationRequests.js
@@ -1,5 +1,4 @@
-var questionnaire_id,
-    requests_array = [];
+var requests_array = [];
 
 $(window).on('load',function(){
   getParticipationRequests();
@@ -29,14 +28,14 @@ function getParticipationRequests()
           {
             out += "<div class='list-group-item col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10' style='border: 2px solid " + types[requests_array[i]['request_type']]['color'] + "' id='ritem" + requests_array[i]['request_id'] + "'>" +
                       "<div class='row'>" +
-                        "<div class='visible-xs col-xs-offset-7 col-xs-3'>" +
-                            "<div style='font-size:15px' class='list-group-item-heading'>" + requests_array[i]['request_date'] + "</div>" +
+                        "<div class='visible-xs col-xs-offset-6 col-xs-6'>" +
+                            "<div style='font-size:15px' class='list-group-item-heading'>" + requests_array[i]['request_date'].split(" ")[0] + "</div>" +
                         "</div>" +
                           "<div class='col-xs-12 col-sm-7 col-md-8'>" +
                               "<div style='font-size:18px' class='list-group-item-heading'><a class='user_name' style='color:black' target='_blank'>" + requests_array[i]['user_name'] + " " + requests_array[i]['user_surname'] + "</a></div>" +
                           "</div>" +
-                          "<div class='hidden-xs col-sm-offset-2 col-sm-2'>" +
-                              "<div style='font-size:15px' class='list-group-item-heading'>" + requests_array[i]['request_date'] + "</div>" +
+                          "<div class='hidden-xs col-sm-offset-2 col-sm-3 col-md-2'>" +
+                              "<div style='font-size:15px' class='list-group-item-heading'>" + requests_array[i]['request_date'].split(" ")[0] + "</div>" +
                           "</div>" +
                       "</div>" +
                       "<div class='row'>" +
@@ -46,7 +45,7 @@ function getParticipationRequests()
                         "<div class='col-xs-12 col-sm-5 col-md-6'>" +
                             "<div style='font-size:17px' > Game: <a class='user_name' style='color:black'>" + requests_array[i]['questionnaire_id'] + "</a></div>" +
                         "</div>" +
-                          "<div class='col-xs-offset-5 col-xs-7 col-sm-offset-4 col-sm-3 col-md-2'>" +
+                          "<div class='col-xs-offset-6 col-xs-6 col-sm-offset-4 col-sm-3 col-md-2'>" +
                             "<button type='button' class='btn btn-success' onclick=\"handleQuestionnaireParticipationRequest(" + requests_array[i]['request_id'] + ",'accept'," + i + ")\"><span class='fa fa-check'></span></button> " +
                             "<button type='button' class='btn btn-danger' onclick=\"handleQuestionnaireParticipationRequest(" + requests_array[i]['request_id'] + ",'decline'," + i + ")\"><span class='fi-x'></span></button>" +
                           "</div>" +
