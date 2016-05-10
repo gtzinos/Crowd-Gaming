fd76123e03e9cc68faeb28ef6782763b30752915
George Tzinos <geotzinos@gmail.com>
2016-05-11 01:10:37 +0300

    {NEW} Questionnaire view page path defined

--- app/view/player/MyQuestionnairesView.php
+++ app/view/player/MyQuestionnairesView.php
@@ -1,9 +1,13 @@
 <?php if($section == "CSS") : ?>
 <?php elseif($section == "JAVASCRIPT") : ?>
-
+<script src="<?php print LinkUtils::generatePublicLink("js/player/MyQuestionnaires.js"); ?>"> </script>
 <?php elseif($section == "MAIN_CONTENT" ) : ?>
   <legend class="text-center header">My Questionnaires</legend>
-
+  <?php
+      echo "  <script>
+                var questionnaire_page = '" . LinkUtils::generatePageLink('questionnaire') . "';
+              </script>";
+  ?>
   <div class="container-fluid">
     <div class="list-group" id="my-questionnaires-list">
 
