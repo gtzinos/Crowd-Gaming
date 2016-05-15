a32f164a3835d42d7e16c6637825c29f80668fb6
George Tzinos <geotzinos@gmail.com>
2016-05-15 10:10:02 +0300

    {NEW} Events (Update, Finish) on jquery countdown clock

--- public/js/common/jquery-clock.js
+++ public/js/common/jquery-clock.js
@@ -5,18 +5,33 @@
 var default_options = {
 
 };
-function show_clock(element,finalDate,strftime="m months %-d days %-H h %M min %S sec",onUpdate = "",onFinish = "")
+// Countdown initialization
+function show_clock(element,finalDate,strftime="%m months %-d days %-H h %M min %S sec",onUpdate = "",onFinish = "")
 {
-  $(element).countdown(finalDate, function(event) {
-    $(this).html(event.strftime(strftime));
+  $(element).countdown(finalDate)
+  //for each update
+  .on('update.countdown', function(event){
+    if (event.offset.totalDays && event.offset.totalDays > 0) {
+      $(this).html(event.strftime("%Dd %H:%M:%S"));
+    }
+    else {
+      $(this).html(event.strftime("%H:%M:%S"));
+    }
+  })
+  //when will finish
+  .on('finish.countdown', function(event) {
+    show_notification("error","Your time expired.",5000);
+    $(this).html(event.strftime("%H:%M:%S"));
+    setTimeout(function() {
+      location.reload(); //back to my questionnaires
+    },5000);
   });
 }
-
 /*
   #No parameters
   YYYY/MM/DD
 */
-function getNow()
+function getToday()
 {
   var month = d.getMonth()+1;
   var day = d.getDate();
