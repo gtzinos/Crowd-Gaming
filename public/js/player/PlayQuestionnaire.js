9ecc9ac17b56cac05e3abdf4d9e04e2b31c5f130
George Tzinos <geotzinos@gmail.com>
2016-05-16 22:14:44 +0300

    {NEW} Click on address and take directions {FIX{ Real address bugs

--- public/js/player/PlayQuestionnaire.js
+++ public/js/player/PlayQuestionnaire.js
@@ -1,10 +1,11 @@
-var address = "";
+var groups = [];
 $(window).on("load",function()
  {
    show_clock("#count-down",moment().add(1441,'minutes').format("YYYY/MM/DD hh:mm:ss"));
    getQuestionGroups();
  });
 
+//get question groups
 function getQuestionGroups()
 {
   $.post(webRoot + "/rest_api/questionnaire/" + questionnaire_id + "/groups",
@@ -15,54 +16,80 @@ function getQuestionGroups()
   {
     if(status == "success")
     {
-    //  alert(JSON.stringify(data));
-      $groups = data["question-group"];
-
-      var i = 0,
-          out = "";
-
-      out = "";
-      for(i=0; i<$groups.length; i++)
-      {
-        (function(i)
-        {
-          $.post("https://maps.googleapis.com/maps/api/geocode/json?address="+ $groups[i]["latitude"] + "," + $groups[i]["longitude"],
-          {
-
-          },
-          function(data,status)
-          {
-            address = data["results"][0]["address_components"][0]["long_name"];
-              out = "<div class='panel panel-info'>" +
-      			      			"<div class='panel-heading text-center'>" +
-      			        			"<p class='panel-title'>" +
-      											"<a data-toggle='collapse' data-parent='#accordion' href='#collapse" + $groups[i].id + "'>" +
-                              $groups[i].name +
-      											"</a>" +
-      										"</p>" +
-      									"</div>" +
-                        "<div id='collapse" + $groups[i].id + "' class='panel-collapse collapse'>" +
-      										"<div class='panel-body'>" +
-      											"<div>Answered " +
-                              $groups[i]["answered-questions"] + "/" + $groups[i]["total-questions"] +
-                            "</div>" +
-                            "<div id='location'>" +
-                              (address.length > 0 ? "<a href='' target='_blank'><span class='fi-map' style='font-size:20px'></span> " + address + "</a>": "<span style='color:red'>No address<span>") +
-                            "</div>" +
-                            "<div class='col-xs-offset-6 col-xs-4 col-sm-offset-9 col-sm-3'>" +
-      												"<button class='btn btn-primary round' target='_blank' type='button' " +
-                              ($groups[i]["answered-questions"] == $groups[i]["total-questions"] ? " disabled>Completed" : ">Play now") +
-                              "</button>"+
-      											"</div>" +
-      										"</div>" +
-      								  "</div>" +
-      								"</div>";
-                      $("#accordion").append(out);
-            });
-        })(i);
-      }
+      groups = data["question-group"];
+      getAddresses();
     }
   });
+}
+
+function getAddresses()
+{
+  var i = 0;
+  var out = "";
+
+  //get addresses from google api
+  for(i=0; i<groups.length; i++)
+  {
+    (function(i)
+    {
+        /*
+          Get real address name
+          #URL : https://maps.googleapis.com/maps/api/geocode/json?address={*latitude,*longitude}
+
+          #No Parameters
+        */
+
+        $.post("https://maps.googleapis.com/maps/api/geocode/json?address="+ (groups[i]["latitude"] != null ? groups[i]["latitude"] + ","  : "")  + (groups[i]["longitude"] != null ? groups[i]["longitude"] : ""),
+        {
+
+        },
+        function(data,status)
+        {
+            groups[i]["address"] = data["results"] != undefined ? data["results"][0]["formatted_address"] : "";
+            if(groups[groups.length-1]["address"] != undefined)
+            {
+              displayData();
+            }
+        });
+     })(i);
+  }
+}
+
+function displayData()
+{
+  out = "";
+  for(i=0; i<groups.length; i++)
+  {
+    (function(i)
+    {
+          out += "<div class='panel panel-info'>" +
+                      "<div class='panel-heading text-center'>" +
+                        "<p class='panel-title'>" +
+                          "<a data-toggle='collapse' data-parent='#accordion' href='#collapse" + groups[i].id + "'>" +
+                            groups[i].name +
+                          "</a>" +
+                        "</p>" +
+                      "</div>" +
+                      "<div id='collapse" + groups[i].id + "' class='panel-collapse collapse'>" +
+                        "<div class='panel-body'>" +
+                          "<div>Answered " +
+                            groups[i]["answered-questions"] + "/" + groups[i]["total-questions"] +
+                          "</div>"+
+                          "<div id='location'>" +
+                                  (groups[i]["address"] ? "<a href='https://www.google.com/maps/dir//" + groups[i]["latitude"] + "," + groups[i]["longitude"] + "' target='_blank'><span class='fi-map' style='font-size:20px'></span> " + groups[i]["address"] + "</a>" : "<span style='color:red'>No address<span>") +
+                          "</div>" +
+                          "<div class='col-xs-offset-6 col-xs-4 col-sm-offset-9 col-sm-3'>" +
+                              "<button class='btn btn-primary round' type='button' " +
+                                (groups[i]["answered-questions"] == groups[i]["total-questions"] ? " disabled>Completed" : ">Play now") +
+                              "</button>"+
+                          "</div>" +
+                      "</div>" +
+                  "</div>" +
+                "</div>";
+
+    })(i);
+    $("#accordion").html(out);
+  }
 }
 
 function calculateDistance()
