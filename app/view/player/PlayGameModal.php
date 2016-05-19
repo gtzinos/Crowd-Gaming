f3fb3cc3c5c5d83cb453b2422bf30957e09484e1
George Tzinos <geotzinos@gmail.com>
2016-05-19 20:55:46 +0300

    {NEW} Buttons removed from design. I will add methods which will append code to dom

--- app/view/player/PlayGameModal.php
+++ app/view/player/PlayGameModal.php
@@ -13,16 +13,7 @@
 				 </div>
 				<!-- Footer fields -->
 				<div class="modal-footer container-fluid">
-						<div class="form-group">
-							 <div class="col-xs-4 col-sm-offset-2 col-sm-3">
-								 <button type="button" class="btn btn-primary btn-md" onclick="sendPublicRequest">Confirm</button>
-							 </div>
-							 <div class="col-xs-3 col-sm-2">
-								 <button type="button" class="btn btn-primary btn-md" data-dismiss="modal" >
-									 Cancel
-								 </button>
-							 </div>
-						</div>
+
 				 </div>
 			 </div>
 		 </div>
