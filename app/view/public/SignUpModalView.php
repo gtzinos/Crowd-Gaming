6961b94c0761a3467ce8c0c2de762d0119aae923
George Tzinos <geotzinos@gmail.com>
2016-06-10 00:25:58 +0300

    {FIX} Sign up spinner position updated.

--- app/view/public/SignUpModalView.php
+++ app/view/public/SignUpModalView.php
@@ -128,6 +128,12 @@
                       </div>
 
                   </div>
+                  <!-- Google recaptcha -->
+                  <div class="form-group">
+										<div class="col-xs-offset-1 col-xs-8 col-sm-offset-3 col-sm-4" >
+												<div id="captcha1" style="transform: scale(0.80,0.85); transform-origin: left top;" class="g-recaptcha"></div>
+										</div>
+									</div>
                   <!-- Spinner and Response Label Field -->
                   <div class="form-group">
                     <div class="col-xs-offset-3 col-xs-8 ">
@@ -135,15 +141,8 @@
                       <div id="signup-spinner">
 
                       </div>
-
                     </div>
                   </div>
-                  <!-- Google recaptcha -->
-                  <div class="form-group">
-										<div class="col-xs-offset-1 col-xs-8 col-sm-offset-3 col-sm-4" >
-												<div id="captcha1" style="transform: scale(0.80,0.85); transform-origin: left top;" class="g-recaptcha"></div>
-										</div>
-									</div>
                   <!-- Register Button Field -->
                   <div class="form-group">
                      <div class="col-xs-offset-3 col-xs-8 col-sm-offset-3">
