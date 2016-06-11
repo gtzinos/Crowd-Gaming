7bebe1fc37f36c7a512913932e9f1aed49ef382f
George Tzinos <geotzinos@gmail.com>
2016-06-11 15:27:54 +0300

    {FIX} Bootstrap carousel on home page
    *All files from folder ./public/res/slider will include into slider
    component

--- app/view/public/HomePageView.php
+++ app/view/public/HomePageView.php
@@ -2,7 +2,7 @@
 <style>
 .carousel-inner > .item > img,
 .carousel-inner > .item > a > img {
-		width: 50%;
+		width: 60%;
 		margin: auto;
 }
 </style>
@@ -12,7 +12,42 @@
 	<div class="container-fluid">
 		<center><h1> Welcome to Crowd Gaming </h1> </center>
 	  <br>
+		<div id = "myCarousel" class = "carousel slide">
+				<!-- Carousel indicators -->
 
+				<?php
+					global $_CONFIG;
+					echo "<ol class='carousel-indicators'>";
+							$images = array_diff(scandir("./res/slider"), array('..', '.'));
+							$counter = 0;
+							foreach($images as $image)
+							{
+								if(!is_dir($image))
+								{
+										 echo "<li data-target='#myCarousel' data-slide-to='" . $counter . "' " . ($counter == 0 ? "class='active'" : "") . "></li>";
+					 			}
+								$counter++;
+							}
+					echo "</ol>";
+					echo "<div class='carousel-inner'>";
+					$counter = 0;
+					foreach($images as $image)
+					{
+						if(!is_dir($image))
+						{
+					      echo "<div class='item " . ($counter == 0 ? "active" : "") . "'>
+										    <img src='" . "res/slider/" . $image . "'>
+								      </div>";
+						}
+						$counter++;
+					}
+					echo "</div>";
+				?>
+		   <!-- Carousel nav -->
+		   <a class = "carousel-control left" href = "#myCarousel" data-slide = "prev">&lsaquo;</a>
+		   <a class = "carousel-control right" href = "#myCarousel" data-slide = "next">&rsaquo;</a>
+
+		</div>
 	</div>
 <!-- https://upload.wikimedia.org/wikipedia/commons/4/47/FPC_stats,_2007-2010.png -->
 
