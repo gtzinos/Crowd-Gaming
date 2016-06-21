1007cdd5839d140d5b5556a29aeb6c78dfe7029c
George Tzinos <geotzinos@gmail.com>
2016-06-21 22:09:17 +0300

    {NEW} Draw chart function defined

--- app/view/player/QuestionnaireScoresModalView.php
+++ app/view/player/QuestionnaireScoresModalView.php
@@ -1,4 +1,5 @@
 <? if($section == "QUESTIONNAIRE_SCORES") : ?>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script src="<?php print LinkUtils::generatePublicLink("js/library/jsPDF/dist/jspdf.min.js"); ?>"></script>
   <script src="<?php print LinkUtils::generatePublicLink("js/player/QuestionnaireResults.js"); ?>"></script>
 	<div class="modal fade" id="questionnaire-results" role="dialog">
@@ -11,17 +12,19 @@
 				 <div class="modal-body container-fluid">
            <div id="results-place"></div>
            <br><br>
+           <div id="charts-place"></div>
+           <br><br>
            <div id="buttons-place">
               <div class="col-xs-12 col-sm-4 col-sm-offset-1">
                 <button id="download-submit" class="form-control" onclick="downloadAsPdf()">Download PDF</button>
               </div>
               <div class="visible-xs"><br><br></div>
               <div class="col-xs-12 col-sm-4">
-                <button id="get-charts-submit" class="form-control" onclick="getCharts()">Get charts</button>
+                <button id="get-charts-submit" class="form-control" onclick="drawChart()">Get charts</button>
               </div>
               <div class="visible-xs"><br><br></div>
               <div class="col-xs-12 col-sm-2">
-                <button id="refresh-results-submit" class="form-control" onclick="refreshTable()">Refresh</button>
+                <button id="refresh-results-submit" class="form-control" onclick="getAllScores()">Refresh</button>
               </div>
            </div>
 				 </div>
