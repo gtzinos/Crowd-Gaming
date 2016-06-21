c9948c7bbdcd6cad0354594ee409e14c9f174d69
George Tzinos <geotzinos@gmail.com>
2016-06-21 20:49:31 +0300

    {NEW} Get all scores function defined

---
+++ public/js/player/QuestionnaireResults.js
@@ -0,0 +1,112 @@
+var scores_array = [];
+
+$(window).on("load",function() {
+  $('#questionnaire-results').on('shown.bs.modal', function (e) {
+    getAllScores();
+  })
+});
+
+function getAllScores()
+{
+  $.ajax(
+  {
+    method: "POST",
+    url: webRoot + "/rest_api/questionnaire/" + questionnaire_id + "/score",
+    data: {}
+  })
+  .done(function(data)
+  {
+    if(data.code == "200")
+    {
+      $("#results-place").html("");
+      var out = "<div class='table-responsive'>" +
+                    "<table class='table'>" +
+                      "<thead>" +
+                        "<tr>" +
+                          "<th>Full Name</th>" +
+                          "<th>Email</th>" +
+                          "<th>Degree</th>" +
+                        "</tr>" +
+                      "</thead>" +
+                      "<tbody>";
+      if(data.scores != null && data.scores.length > 0)
+      {
+        $.each(data.scores, function(i,group) {
+            $.each(group, function(j, userstats) {
+              if(scores_array[userstats.email] != undefined)
+              {
+                scores_array[userstats.email] = intval(scores_array[userstats.email].score) + intval(userstats.score);
+              }
+              else {
+                scores_array[userstats.email] = { userstats };
+              }
+            });
+        });
+
+        $.each(scores_array, function(i, user) {
+          out += "<tr>" +
+                      "<td>" + user["user-surname"] + " " + user["user-email"] + "</td>" +
+                      "<td>" + user["user-email"] + "</td>" +
+                      "<td>" + user["score"] + "</td>" +
+                  "</tr>";
+        });
+      }
+      out += "</tbody>" +
+            "</table>" +
+        "</div>";
+      $("#results-place").html(out);
+    }
+  })
+  .fail(function(xhr,error){
+    var response = JSON.parse(xhr.responseText);
+    switch(response.code)
+    {
+      case "604":
+        show_notification("error",response.message,4000);
+        break;
+      default:
+        displayServerResponseError(xhr,error);
+        break;
+    }
+  });
+}
+
+   function demoFromHTML() {
+       var pdf = new jsPDF('p', 'pt', 'letter');
+       // source can be HTML-formatted string, or a reference
+       // to an actual DOM element from which the text will be scraped.
+       source = $('#content').html();
+
+       // we support special element handlers. Register them with jQuery-style
+       // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+       // There is no support for any other type of selectors
+       // (class, of compound) at this time.
+       specialElementHandlers = {
+           // element with id of "bypass" - jQuery style selector
+           '#bypassme': function (element, renderer) {
+               // true = "handled elsewhere, bypass text extraction"
+               return true
+           }
+       };
+       margins = {
+           top: 80,
+           bottom: 60,
+           left: 40,
+           width: 522
+       };
+       // all coords and widths are in jsPDF instance's declared units
+       // 'inches' in this case
+       pdf.fromHTML(
+       source, // HTML string or DOM elem ref.
+       margins.left, // x coord
+       margins.top, { // y coord
+           'width': margins.width, // max width of content on PDF
+           'elementHandlers': specialElementHandlers
+       },
+
+       function (dispose) {
+           // dispose: object with X, Y of the last line add to the PDF
+           //          this allow the insertion of new lines after html
+           pdf.save('Test.pdf');
+       }, margins);
+   }
