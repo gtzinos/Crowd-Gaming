54d3c3ae4d9f2a504557d5c7d1da2b1c88a8bd42
George Tzinos <geotzinos@gmail.com>
2016-10-23 14:56:52 +0300

    FIX: config_general.php

--- app/config/config_general.php
+++ app/config/config_general.php
@@ -9,7 +9,7 @@
 		Leave empty if the public folder IS the root.
 		WEB_ROOT Must end with '/' if exists.
 	 */
-	$_CONFIG["WEB_ROOT"] = "./Crowd-Gaming/public/";
+	$_CONFIG["WEB_ROOT"] = "./";
 	/*
 		Short application name
 	*/
